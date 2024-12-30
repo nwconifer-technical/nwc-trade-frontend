49795ffa8cec6d114269e190e399e1f53c923752
Alicolliar <alicolliar@gmail.com>
2024-12-30 23:30:25 +0000

    Header cash readout

--- src/app/HeaderUnit.jsx
+++ src/app/HeaderUnit.jsx
@@ -3,11 +3,23 @@ import Image from "next/image";
 import Link from "next/link";
 import signout from "./access/signout";
 
+const quickCash = async (name) => {
+  const API_ROUTE = process.env.API_ADDRESS;
+  const theFetched = await fetch(`${API_ROUTE}/cash/quick/${name}`);
+  if (theFetched.status != 200) {
+    return {
+      CashInHand: 0,
+    };
+  }
+  return await theFetched.json();
+};
+
 const Navbar = async (props) => {
   var userPerms = "";
   if (props.loggedIn) {
     userPerms = props.sessionCookie.permission;
   }
+  const cashVal = await quickCash(props.sessionCookie.name);
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -32,7 +44,7 @@ const Navbar = async (props) => {
         ) : (
           ""
         )}
-        {userPerms == "trader" || userPerms == "admin" ? (
+        {userPerms != "citizen" ? (
           <Link href={"/region"} className="navbar-item">
             My Region
           </Link>
@@ -41,6 +53,14 @@ const Navbar = async (props) => {
         )}
       </div>
       <div className="navbar-end">
+        {props.loggedIn ? (
+          <div className="navbar-item">
+            Hello, {props.sessionCookie.name}. <br />
+            You have ${cashVal.CashInHand} in cash
+          </div>
+        ) : (
+          ""
+        )}
         <div className="navbar-item">
           <div className="buttons">
             {props.loggedIn ? (
